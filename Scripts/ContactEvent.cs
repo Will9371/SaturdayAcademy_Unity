493e7d0a451fac1a3cb1288ca8b20c9c0f5e782f
WillPetillo <will.petillo@gmail.com>
2019-07-08 23:51:39 -0700

    Added ContactEvent
    Use this to fire events when a collision occurs.  Supports triggers and non-triggers.  Use the object of interest field to only fire on collision with a specific scene object, or leave blank to fire on any collision

---
+++ Scripts/ContactEvent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ContactEvent : MonoBehaviour
+{
+    [SerializeField] GameObject objectOfInterest;
+    [SerializeField] UnityEvent OnEnter, OnExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (objectOfInterest != null && objectOfInterest != other.gameObject)
+            return;
+
+        OnEnter.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (objectOfInterest != null && objectOfInterest != other.gameObject)
+            return;
+
+        OnExit.Invoke();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        OnTriggerEnter(other.collider);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        OnTriggerExit(other.collider);
+    }
+} 
